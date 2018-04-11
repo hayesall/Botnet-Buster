f560ac43241fc34396fc08002cfd148841e24a85
batflyer <alexander@batflyer.net>
2018-04-11 18:37:24 -0500

    Adding function  headers for implementing algorithms 1 and 2 from the Discriminative Boosted Bayes Nets paper.

--- src/disc_boost.py
+++ src/disc_boost.py
@@ -33,7 +33,41 @@ __maintainer__ = "Alexander L. Hayes (@batflyer)"
 __email__ = "alexander@batflyer.net"
 __status__ = "Prototype"
 
+def LearnDB2N(T, F):
+    """
+    Learns a series of softmax regression trees from targets, features, and
+    an ordering of variables.
 
+    Based on Algorithm 1 in "Discriminative Boosted Bayes Networks for Learn-
+    ing Multiple Cardiovascular Procedures", but makes an assumption that
+    T specifies both the elements and their ordering (i.e. instead of specif-
+    ying both T and O variables, the ordering set in O is assumed to be the
+    the same as it would be in T).
+
+    @method LearnDB2N
+    @param  {list}      T           ordered set of targets for the dataset
+    @param  {list}      F           set of observed features
+    @return {object}    BN
+    """
+    pass
+
+def SFGBoost():
+    """
+    Performs softmax boosting.
+
+    Based on Algorithm 2 in "Discriminative Boosted Bayes Networks for Learn-
+    ing Multiple Cardiovascular Procedures".
+
+    !!! This is not an actual implementation of SFGBoost, rather this is
+        a series of wrappers which interact with a BoostSRL jar file.
+
+        Visit BoostSRL on GitHub [1] or the documentation on the website [2]
+        [1]: https://github.com/starling-lab/BoostSRL
+        [2]: https://starling.utdallas.edu/software/boostsrl/wiki/cost-sensitive-srl/
+
+    @method SFGBoost
+    """
+    pass
 
 if __name__ == '__main__':
-    print('Hello World')
+    pass
