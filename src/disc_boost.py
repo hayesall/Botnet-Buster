c5d0d3154d89f6f0de81c076df2629dd54ec935d
batflyer <alexander@batflyer.net>
2018-04-11 18:59:42 -0500

    Adding a more thorough example and description for LearnDB2N

--- src/disc_boost.py
+++ src/disc_boost.py
@@ -35,8 +35,25 @@ __status__ = "Prototype"
 
 def LearnDB2N(T, F):
     """
-    Learns a series of softmax regression trees from targets, features, and
-    an ordering of variables.
+    Learns Bayes Network from an ordered set of targets and observed features.
+
+    Description:
+    Given a variable ordering of targets and observations, the goal is to learn
+    a joint probability of targets and the variables which relate to them via
+    Bayes Network. Consider the following dataset:
+
+    +--------+--------+--------+-----+-------+----+
+    | Person | Test_1 | Test_2 | EKG | ANGIO | VR |
+    +--------+--------+--------+-----+-------+----+
+    |   p1   |   0    |    1   |  1  |   1   |  1 |
+    +--------+--------+--------+-----+-------+----+
+    |   p2   |   1    |    1   |  1  |   1   |  0 |
+    +--------+--------+--------+-----+-------+----+
+
+    Test_1 and Test_2 are tests done on them at a certain point in time.
+    EKG, ANGIO, and VR are procedures they received later in life.
+
+    P(E, A, V, T) ~= P(E | T) P(A | E, T) P(V | E, A, T)
 
     Based on Algorithm 1 in "Discriminative Boosted Bayes Networks for Learn-
     ing Multiple Cardiovascular Procedures", but makes an assumption that
@@ -45,9 +62,16 @@ def LearnDB2N(T, F):
     the same as it would be in T).
 
     @method LearnDB2N
-    @param  {list}      T           ordered set of targets for the dataset
-    @param  {list}      F           set of observed features
+    @param  {list}      T           ordered set of targets for the dataset,
+                                    represented as a list of strings
+    @param  {list}      F           set of observed features, also represen-
+                                    ted as a list of strings
     @return {object}    BN
+
+    Example:
+    >>> targets = ['ekg', 'angio', 'vr']
+    >>> observs = ['t1', t2]
+    >>> BayesNet = LearnDB2N(targets, observs)
     """
     pass
 
