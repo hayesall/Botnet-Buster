54099cba25fe42098f636b861c38facca43c248f
batflyer <alexander@batflyer.net>
2018-04-19 11:35:54 -0500

    Adding Options section to the download.sh script. Reindenting the file since Atom does so every time I open it.

--- download.sh
+++ download.sh
@@ -7,27 +7,31 @@
 #      Email: alexander.hayes@utdallas.edu
 #      Copyright: Alexander L. Hayes and Brian Ricks
 #      License: GPL-v3
-#  
+#
 #  Description:
 #      It's generally bad practice to push large data
 #      sets to GitHub. Download the data separately.
-#  
+#
 #  Usage:
 #      $ bash download.sh
-#  
+#
+#  Options:
+#      -h     Display this help message and exit.
+#      -s     Scripted mode. Do not prompt user to validate download.
+#
 #  License:
 #  Copyright (c) 2018 Alexander L. Hayes and Brian Ricks
-#  
+#
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU General Public License for more details.
-#  
+#
 #  See <http://www.gnu.org/licenses/> or the license in the base of
 #  this repository.
 
@@ -37,7 +41,7 @@ while getopts "hs" o; do
     case ${o} in
 	h)
 	    # Help
-	    head -n 32 $0 | tail -n +3 | sed 's/#  //'
+	    head -n 36 $0 | tail -n +3 | sed 's/#//'
 	    exit 0
 	    ;;
 	s)
@@ -54,7 +58,7 @@ done
 function Downloader() {
     # Download the .tar.bz of the CTU-13-Dataset using curl.
     curl -k -L https://mcfp.felk.cvut.cz/publicDatasets/CTU-13-Dataset/CTU-13-Dataset.tar.bz2 > CTU-13-Dataset.tar.bz2
-    
+
     # Use unar to extract from the archive.
     unar CTU-13-Dataset.tar.bz2
 }
@@ -65,30 +69,26 @@ function Cleanup() {
 }
 
 function Main() {
-    if [[ ! $scriptmode ]]; then
-	
-	printf "Download CTU-13-Dataset? (1.9 GB) [y/n] "
-	read -r download
-	
-	if [[ "$download" = "y" ]]; then
-	    Downloader
-
-	    printf "Remove the .tar.bz2 archive? [y/n] "
-	    read -r removal
-
-	    if [[ "$removal" = "y" ]]; then
-		Cleanup
-	    else
-		echo "Did not remove CTU-13-Dataset.tar.bz2"
-	    fi
-	    
-	else
-	    echo "Exited without downloading."
-	    exit 0
-	fi
+  if [[ ! $scriptmode ]]; then
+    printf "Download CTU-13-Dataset? (1.9 GB) [y/n] "
+    read -r download
+    if [[ "$download" = "y" ]]; then
+      Downloader
+      printf "Remove the .tar.bz2 archive? [y/n] "
+      read -r removal
+      if [[ "$removal" = "y" ]]; then
+        Cleanup
+      else
+        echo "Did not remove CTU-13-Dataset.tar.bz2"
+      fi
     else
-	Downloader
+      echo "Exited without downloading."
+      exit 0
     fi
+  else
+    Downloader
+  fi
 }
 
 Main
+exit 0
