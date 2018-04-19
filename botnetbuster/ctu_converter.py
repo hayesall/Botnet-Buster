c9f492f8699da6022216a87b399919ad75c3c6aa
batflyer <alexander@batflyer.net>
2018-04-19 12:04:41 -0500

    Renamed the src folder to botnetbuster (hopefully making import statements make more sense in the future).

--- botnetbuster/ctu_converter.py
+++ botnetbuster/ctu_converter.py
@@ -109,7 +109,7 @@ def dataframe_to_relations(path_to_file, df, verbosity=False):
         ret = ''
         ret += type
         ret += '("'
-        ret += id.replace(' ','')
+        ret += id.replace(' ', '')
         ret += '","'
         ret += value.replace(' ', '')
         ret += '").'
@@ -218,7 +218,7 @@ def main():
     else:
         v = False
 
-    flow = binetflow_converter(args.file, dropCols=['StartTime'], verbosity=v)
+    flow = binetflow_converter(args.file, dropCols=['StartTime', 'SrcAddr', 'DstAddr', 'sTos', 'dTos'], verbosity=v)
     dataframe_to_relations(args.output, flow, verbosity=v)
 
 if __name__ == '__main__':
